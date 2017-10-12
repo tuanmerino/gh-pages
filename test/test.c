d120afdf9d94ccc7e5285f4d7a82319cc123da39
Nguyễn Tuấn Anh <iicodonduoimuaii@gmail.com>
2017-10-13 06:22:36 +0700

    Update test.c

--- test/test.c
+++ test/test.c
@@ -1,19 +1,31 @@
+
+#include <stdio.h>
 #include <math.h>
+
+int check1(int n,int d){
+	int x;
+	x=pow(10,d);
+	if(n%x !=n){
+		return check1(n,d+1);
+	}
+	return d-1;
+}
+int check2(int n,int d,int s){
+	int x,du;
+
+	if (d==-1){
+		return 0;
+	}
+	x=pow(10,d);
+	du=n%x;
+	return ((int)n/x)*(pow(10,s))+check2(du,d-1,s+1);
+
+}
 int main(){
-    long long n;
-    int i;
-    int d;
-    scanf("%lld",&n);
-    for (i=2;i<n;i++){
-
-        if (n%i==0){
-            d=0;
-            printf("%d khong phai so nguyen to",n);
-            break;
-        }
-
-    }
-    if (d==1){printf("%d la so nguyen to",n);
-
-    }
+	int n;
+	int dem;
+	scanf("%d",&n);
+	dem=check1(n,0);
+	printf("%d\n",dem);
+	printf("%d",check2(n,dem,0));
 }
